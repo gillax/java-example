10dedee5961c33ce31e556084abb72bca6ec366f
gillax <gillax@gmail.com>
2016-09-08 15:10:47 +0900

    LinkedHashSetを使えば順序そのままで重複だけを除外できる

--- guava/src/main/java/PartitionedListWithoutDuplication.java
+++ guava/src/main/java/PartitionedListWithoutDuplication.java
@@ -8,12 +8,16 @@ public class PartitionedListWithoutDuplication {
 		List<String> favBands = Lists.newArrayList("Lotus","STS9","Disco Biscuits","New Deal","Flying Lotus","STS9");
 		System.out.println(favBands);
 
-		// without duplicate
+		// without duplicate(HashSet)
 		List<String> favBandWithoutDups = Lists.newArrayList(Sets.newHashSet(favBands));
 		System.out.println(favBandWithoutDups);
 
+		// without duplicate and keep order(LinkedHashSet)
+		List<String> favBandWithoutDupsKeepOrder = Lists.newArrayList(Sets.newLinkedHashSet(favBands));
+		System.out.println(favBandWithoutDupsKeepOrder);
+
 		// partitioned
-		List<List<String>> partitionedLists = Lists.partition(favBandWithoutDups, 2);
+		List<List<String>> partitionedLists = Lists.partition(favBandWithoutDupsKeepOrder, 2);
 		System.out.println(partitionedLists);
 	}
 }
